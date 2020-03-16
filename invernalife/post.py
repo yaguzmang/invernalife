bc831ad6e3187615008c2a33ed8dafbfa5a4a338
yaguzmang <41337901+yaguzmang@users.noreply.github.com>
2020-03-16 12:24:50 -0500

    Delete post.py

--- invernalife/post.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-payload = {'temp':28.5,'hum':85.5,'ph':7.5}
-payloadJson = json.dumps(payload)
-r = requests.post("http://localhost:3000/api/", headers = {'Content-Type': 'application/json'}, data=payloadJson)
-r_dict = r.json()
-print(r_dict)
